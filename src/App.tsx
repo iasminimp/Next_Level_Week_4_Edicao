b2f0670db9cc2bd94321718c6e572274667e04a6
Iasmin Marques Pereira <iasminimp7@gmail.com>
2021-03-24 17:49:07 -0300

    Upando o projeto Next Level Week 4

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {Button} from './components/Button'
+import { ExperienceBar } from './components/ExperienceBar';
+
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="container">
+     <ExperienceBar />
+      
+    </div>
+    
+  );
+}
+export default App;
